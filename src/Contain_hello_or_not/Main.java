bbd4bd20042fab4a07305cef87f8083b3b63f059
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2020-04-29 17:24:06 +0530

    After solving...

--- src/Contain_hello_or_not/Main.java
+++ src/Contain_hello_or_not/Main.java
@@ -11,5 +11,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        Scanner obj = new Scanner(System.in);
+        String s = obj.nextLine();
+        StringBuffer h = new StringBuffer("");
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'h' || s.charAt(i) == 'H') {
+                h.append('h');
+                k = i;
+                break;
+            }
+        }
+        for (int i = k + 1; i < s.length(); i++) {
+            if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
+                h.append('e');
+                k = i;
+                break;
+            }
+        }
+        for (int i = k + 1; i < s.length(); i++) {
+            if (s.charAt(i) == 'l' || s.charAt(i) == 'L') {
+                h.append('l');
+                k = i;
+                break;
+            }
+        }
+        for (int i = k + 1; i < s.length(); i++) {
+            if (s.charAt(i) == 'l' || s.charAt(i) == 'L') {
+                h.append('l');
+                k = i;
+                break;
+            }
+        }
+        for (int i = k + 1; i < s.length(); i++) {
+            if (s.charAt(i) == 'o' || s.charAt(i) == 'o') {
+                h.append('o');
+                k = i;
+                break;
+            }
+        }
+
+
     }
 }
